abae56c91272dbc81b78a424c381d4757c645b1a
Twedmark <axel.twedmark@hotmail.com>
2022-04-26 12:40:20 +0200

    #33 added login with a check

--- src/views/Admin.js
+++ src/views/Admin.js
@@ -1,8 +1,23 @@
-import './Admin.css';
-import Header from '../components/Header';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react'
 
+import Header from '../components/Header';
+import './Admin.css';
 
 function Admin(){
+  const loggedInUser = useSelector((state) => { return state.loggedInUser});
+  const navigate = useNavigate();
+
+  function cheackLoggin(){
+    if(loggedInUser === null){
+      navigate('/home')
+    }
+  }
+
+  useEffect(() => {
+    cheackLoggin();
+  }, []);
 
   return(
     <section>
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,12 +1,45 @@
-import './Login.css'
-import Header from '../components/Header'
+import { useState, useRef} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setLoggedInUser } from '../actions/userActions';
+
+import Header from '../components/Header';
+import './Login.css';
+
 
 function Login(){
+  const navigate = useNavigate();
+  const emailInput = useRef(null),
+        passwordInput = useRef(null);
+  const users = useSelector((state) => { return state.users}),
+        dispatch = useDispatch();
+
+
+
+
+async function handleLoggin(){
+  if(emailInput.current.value === "" || passwordInput.current.value === ""){
+    alert("fyll i fälten");
+  }
+  let user = users.find(user => user.email === emailInput.current.value && user.password === passwordInput.current.value)
+  if(user){
+    await dispatch(setLoggedInUser(user));
+    navigate('/admin');
+  }else{
+    alert("Login failed");
+}
+}
+
 
   return(
     <section>
     <Header />
     <p>login</p>
+    <section>
+      <input type="text" ref={ emailInput }  placeholder="Email"/>
+      <input type="text" ref={ passwordInput } placeholder="Password"/>
+      <button onClick={ handleLoggin }>loggin</button>
+    </section>
     </section>
   )
 }
