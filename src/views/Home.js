e425cce5e68e9cf74659bcd1474f6bb5936e8f1f
Nadia ben kassem <90767449+nadiasGame@users.noreply.github.com>
2022-04-27 13:04:26 +0200

    Banners

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,7 +3,9 @@ import { useSelector } from 'react-redux'
 import Header from '../components/Header';
 import Book from '../components/book'
 import { useRef, useState } from 'react';
-
+import Picture1 from '../Assets/05a4cac6953fed0c2c835324f3fdf92d.jpeg'
+import Picture2 from '../Assets/black_button_books_-_big_book_of_faces_thumb.jpeg'
+import Picture3 from '../Assets/nedladdning.jpeg'
 function Home(){
   const booksItem = useSelector((state) => { return state.books})
   const [query, setQuery] = useState("")
@@ -29,6 +31,18 @@ function Home(){
   return(
     <section>
     <Header />
+    <div id="container">
+   
+  
+    <div className="photobanner">
+    
+    	<img className="first" src={Picture1} alt="books" />
+    	<img src={Picture2} alt="books" />
+    	<img src={Picture3} alt="books" />
+ 
+    </div>
+</div>
+
       <p>Home</p>
       <input type="text" placeholder="Search" ref={searchField} onChange={event => setQuery(event.target.value)} />
       <section className="BooksWrapper">
@@ -40,3 +54,7 @@ function Home(){
 }
 
 export default Home;
+
+
+
+
