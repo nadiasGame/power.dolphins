a8d2539363925c8928ad34de761f6bde749f29f3
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2022-04-28 11:08:06 +0200

    semi done for #69

--- src/views/Admin.js
+++ src/views/Admin.js
@@ -1,15 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react';
-import { addBook, editBook } from '../actions/bookActions';
 import { addUser, editUser } from '../actions/userActions'
+
+import AddAndEditBook from '../components/AddAndEditBook';
 
 import Header from '../components/Header';
 import './Admin.css';
 
 function Admin(){
   const loggedInUser = useSelector((state) => { return state.loggedInUser}),
-        books = useSelector((state) => { return state.books}),
         users = useSelector((state) => { return state.users}),
         dispatch = useDispatch(),
         navigate = useNavigate(),
@@ -18,16 +18,7 @@ function Admin(){
         passwordInput = useRef(null),
         keycardNumberInput = useRef(null),
         cardPINInput = useRef(null),
-        title = useRef(""),
-        author = useRef(""),
-        description = useRef(""),
-        rating = useRef(""),
-        quantity = useRef(""),
-        price = useRef(""),
-        image = useRef(""),
-        searchForBook = useRef(""),
         [ newUser, setNewUser ] = useState(),
-        [ editingBook, setEditingBook ] = useState(false),
         [ editUserState, setEditUserState ] = useState({
               name: "",
               email: "",
@@ -42,49 +33,6 @@ function Admin(){
     if(loggedInUser === null){
       navigate('/home')
     }
-  }
-
-  function getAndInputBook(){
-    var book = books.find(book => book.id === Number(searchForBook.current.value));
-    if(book){
-      title.current.value = book.title;
-      author.current.value = book.author;
-      description.current.value = book.description;
-      rating.current.value = book.rating;
-      quantity.current.value = book.quantity;
-      price.current.value = book.price;
-      image.current.value = book.imageLink;
-    }
-    setEditingBook(true);
-  }
-
-  function addBookHandler() {
-    var book = {
-      title: title.current.value,
-      author: author.current.value,
-      description: description.current.value,
-      rating: rating.current.value,
-      quantity: quantity.current.value,
-      price: price.current.value,
-      imageLink: image.current.value
-    }
-
-    if(editingBook){
-      book.id = Number(searchForBook.current.value);
-      dispatch(editBook(book));
-      alert("Boken är redigerad!");
-      setEditingBook(false);
-    } else {
-      book.id = books.length + 1;
-      dispatch(addBook(book));
-      alert("Boken är tillagd!");
-    }
-    title.current.value = ""; author.current.value = ""; description.current.value = ""; rating.current.value = ""; quantity.current.value = ""; price.current.value = ""; image.current.value = "";
-  }
-
-  function cancelEditingBook(){
-    setEditingBook(false);
-    title.current.value = ""; author.current.value = ""; description.current.value = ""; rating.current.value = ""; quantity.current.value = ""; price.current.value = ""; image.current.value = "";
   }
 
   useEffect(() => {
@@ -136,23 +84,7 @@ function Admin(){
       <p>Admin</p>
       <div className='addDiv'>
 
-        <div className='addBook'>
-          <input className='searchForBook' type='text' placeholder='Search for a book by id' ref={searchForBook} />
-          <button onClick={getAndInputBook}>Search</button>
-          {editingBook ? <p>Editing book</p> : <p>Adding book</p>}
-
-          <input className='addBookTitle' placeholder='Title' ref={title}></input>
-          <input className='addBookAuthor' placeholder='Author' ref={author}></input>
-          <input className='addBookDescription' placeholder='Description' ref={description}></input>
-          <input className='addBookRating' placeholder='Rating' ref={rating}></input>
-          <input className='addBookQuantity' placeholder='Quantity' ref={quantity}></input>
-          <input className='addBookPrice' placeholder='Price' ref={price}></input>
-
-          <input className="addBookImage" placeholder="Image Link" ref={image}></input>
-          <button onClick={addBookHandler}>{editingBook ? "Edit" : "Add"} book</button>
-
-          {editingBook ? <button onClick={cancelEditingBook}>Cancel</button> : null}
-        </div>
+        <AddAndEditBook />
 
         <section className="addUserForm" >
           <label>Namn:
